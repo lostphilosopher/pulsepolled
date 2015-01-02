8e1d02729e8ec1d4b84f08bcb5b0252d61a7184f
lostphilosopher <wandersen02@gmail.com>
2015-01-02 15:53:34 -0600

    DB test page

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,14 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('test/{feature?}', function($feature = 'db')
+{
+    switch ($feature) {
+        case 'db':
+            return View::make('test.db');
+            break;
+        default:
+            return View::make('hello');    
+    }    
+});
