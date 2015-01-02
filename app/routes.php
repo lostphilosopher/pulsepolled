73408e936fb8e0793a257cddf6e0e00c7b0c5ddd
lostphilosopher <wandersen02@gmail.com>
2015-01-02 16:24:08 -0600

    Raw MongoDB / MongoLab connection test

--- app/routes.php
+++ app/routes.php
@@ -20,8 +20,28 @@ Route::get('test/{feature?}', function($feature = 'db')
 {
     switch ($feature) {
         case 'db':
-            return View::make('tests.db');
+
+	        // Get the mongoDB name from env (heroku config)
+		    // SOURCE: https://gist.github.com/pedro/1288447
+	        // @todo: Use https://github.com/jenssegers/laravel-mongodb or altenative
+		    $mongoUri   = parse_url(getenv('MONGOLAB_URI'));
+		    $dbName     = str_replace('/', '', $mongoUri['path']);
+		    // Connect to mongoDB instance and access the access collection
+		    $mongo      = new Mongo(getenv('MONGOLAB_URI'));
+		    $database   = $mongo->$dbName;
+		    $collection = $database->access;
+		    // Insert a document into the access collection
+		    $visit = array( 'ip' => $_SERVER['HTTP_X_FORWARDED_FOR'] );
+		    $collection->insert($visit);
+		    // Print all the existing documents
+		    $views = $collection->find();
+		    // Close / Disconnect the MongoDB connection
+		    $mongo->close();
+
+            return View::make('tests.db', array('views' => $views));
+
             break;
+
         default:
             return View::make('hello');    
     }    
--- app/views/tests/db.php
+++ app/views/tests/db.php
@@ -1,1 +1,9 @@
 <H1>TESTING DB CONNECTION AND I/O...</H1>
+
+<ul>
+<?php
+	foreach ($views as $view) {
+		echo '<li>' . $view->ip . '</li>';
+	}
+?>
+</ul>
